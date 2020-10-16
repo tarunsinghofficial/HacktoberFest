04f0577d6830c0422e9fa59403247231fbf6435b
ajitamishra <44499838+ajitamishra@users.noreply.github.com>
2020-10-16 19:19:20 +0530

    Create BigInteger.java
    This program uses BigInteger Library to compute large factorials

---
+++ BigInteger.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.math.BigInteger;
+class test
+{
+    static int fact(int n)
+    {
+        if(n==0)
+        return 1;
+        else
+        return n*fact(n-1);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t;
+        t=sc.nextInt();
+        while(t-->0)
+        {
+            Integer n=sc.nextInt();
+           BigInteger b =BigInteger.valueOf(1);
+            for(Integer i=1;i<=n;i++)
+            b=b.multiply(BigInteger.valueOf(i));
+            System.out.println(b);
+        }
+        
+    }
+}
