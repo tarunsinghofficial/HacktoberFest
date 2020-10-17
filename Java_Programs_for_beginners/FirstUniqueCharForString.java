c4ccc7fe0323bcf7303ce2b06b9cca57a78dc349
Noopur Agarwal <52702093+noopurAg@users.noreply.github.com>
2020-10-17 09:28:04 -0700

    first unique char
    First -commit for finding first unique char for a string

---
+++ Java_Programs_for_beginners/FirstUniqueCharForString.java
@@ -0,0 +1,30 @@
+// LANGUAGE: Javas //Getter Setter
+// ENV: Java 8 
+// AUTHOR: Noopur Agarwal
+// GITHUB: https://github.com/noopurAg
+
+import java.util.* ; 
+class FirstUniqueCharForString {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)) 
+                map.put(c,map.get(c)+1);
+             else
+                 map.put(c,1);
+        }
+        for(int i=0;i<s.length();i++){
+           if(map.get(s.charAt(i))==1)
+               return i;
+        }
+           return -1; 
+    }
+
+    public static void main(String args[]){
+
+        FirstUniqueCharForString fchar = new FirstUniqueCharForString();
+        String msg = "hachtoberfest-hack";
+        int indexofChar = fchar.firstUniqChar(msg);
+        System.out.println("First uniq character is ----> '"+ msg.charAt(indexofChar)+ "' loacated at "+ indexofChar );
+    }
+}
