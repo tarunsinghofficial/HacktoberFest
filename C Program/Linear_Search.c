141f49d708e5ba5252dbb753c59fe535dda27f73
kunalAkhade <82122582+kunalAkhade@users.noreply.github.com>
2021-10-01 23:02:13 +0530

    Update Linear_Search.c
    Comments added for each line for comprehensive understanding of the code.

--- C Program/Linear_Search.c
+++ C Program/Linear_Search.c
@@ -7,23 +7,27 @@
 int search(int arr[], int n, int x)
 {
     int i;
-    for (i = 0; i < n; i++)
-        if (arr[i] == x)
-            return i;
-    return -1;
+    for (i = 0; i < n; i++)     // Perfoming the if operation for n times. 
+        if (arr[i] == x)        // Checking if element at ith index is equal to x.
+            return i;           //  if arr[i]=x i.e Element found, then return the i value as well as return control of the program to the main function.
+    return -1;                 // if x is not found in the array, return -1 value as well as return control of the program to the main function.
 }
  
 // Driver code
 int main(void)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = { 2, 3, 4, 10, 40 };       // Arrayand declaration and initialization 
+    int x = 10;                            // x is the element to be found in the array
+    int n = sizeof(arr) / sizeof(arr[0]);  // n is the size of array 'arr[]'
    
-    // Function call
-    int result = search(arr, n, x);
-    (result == -1)
-        ? printf("Not Present")
-        : printf("Present at index %d", result);
-    return 0;
+    
+    int result = search(arr, n, x);        // Function call
+    (result == -1) 
+        ? printf("Not Present")           // if result = -1 i.e Element is not found, this printf function will get executed.
+        : printf("Present at index %d", result); // Otherwise, this printf function will get executed.
+    return 0;  // End of main function.
 }
+
+Time Complexity:
+Worst Case - O(n).
+Best Case - O(1). 
