0beccd33bf8a5a794bb9232cd0f607ed3cd2785b
Wanderson Trindade Vitorino <wandersontrindadevitorino@gmail.com>
2021-10-04 20:55:25 -0300

    Add files via upload

---
+++ Java Print/Dr4E7A2CandidatoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr4E7A2CandidatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr4E7A2CandidatoApplication.class, args);
+	}
+
+}
