5a21fbe17ca91d568b1a29ab9d10eb3a91869269
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2021-10-07 19:56:52 +0530

    Create currency.java

---
+++ Java_Programs_for_beginners/currency.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.*;
+import java.text.*;
+
+class main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en","IN")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
