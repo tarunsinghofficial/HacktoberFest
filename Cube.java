2c10f9c83c21eb3fa2297d01ef14fa02cb995b8b
Fadel Muhammad <57484725+FadellMuhammad@users.noreply.github.com>
2021-10-08 13:10:56 +0700

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,10 @@
+public class kubus {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
