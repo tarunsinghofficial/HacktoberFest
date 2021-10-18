124ab277e4319229bbd5a139cb9358577efc6fed
heckingyou <92439742+heckingyou@users.noreply.github.com>
2021-10-19 01:58:11 +0530

    Update SumOfElementsOfArray.c

--- C Program/SumOfElementsOfArray.c
+++ C Program/SumOfElementsOfArray.c
@@ -1,17 +1,17 @@
 //A c program that simply takes elements of the array from the user and finds the sum of these elements.
 
 #include<stdio.h>
-void main()
+void main(void)
 {
     int n,i,sum=0,arr[20];
 
     printf("Enter the size of array: ");
     scanf("%d",&n);
 
-    printf("Enter %d elements in the array: ",n);
-    for(i=0;i<n;i++)
+    printf("\nEnter %d elements in the array: ",n);
+    for(i=0; i<n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]); //taking user input in the array.
         sum=sum+arr[i];
     }
 
