8a749e1f4666d48c4cefd155976f165d4ebe47f4
Birendrakumarmahto <60342800+Birendrakumarmahto@users.noreply.github.com>
2021-10-27 14:53:00 +0530

    update Awal.java
    add code to print pattern

--- Java Print/src/com/hanifz/modul1/Awal.java
+++ Java Print/src/com/hanifz/modul1/Awal.java
@@ -5,5 +5,24 @@ class Main
     {
         System.out.println("Hello World!");
         System.out.println("This is Pabak Kundu, editing it.My age is "+19);
+        
+        for(int i = 1; i<=5 ; i++)
+        {
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.println(j+" ");
+            }
+            System.out.println();
+        }
+        
+        //add pattern code
+        /*
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+        */
     }
 }
+
