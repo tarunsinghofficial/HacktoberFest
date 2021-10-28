7be36159e2443815bb531423efb4d6fbc8ed972b
Joel Kanyi <joelkanyi98@gmail.com>
2021-10-28 05:41:15 +0300

    Create forEach.kt

---
+++ kotlin-programs/forEach.kt
@@ -0,0 +1,7 @@
+        val listOfNumbers = arrayOf(1,2,3,4,5,6,7,8)
+
+        listOfNumbers.forEach { number ->
+            if (number%2==0){
+                print("$number is even")
+            }
+        }
