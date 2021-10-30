feb80e44dbe4904648dc58a419370ed15309d23c
IamGiel <lfagel84@gmail.com>
2021-10-29 22:54:23 -0400

    please accept my PR, adding levenshtein distance to check similarity between strings

---
+++ Levenshtein_distance.js/levenshtein_distance.js
@@ -0,0 +1,49 @@
+// Levenshtein distance
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+  }
+  
+  function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+  
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+      var lastValue = i;
+      for (var j = 0; j <= s2.length; j++) {
+        if (i == 0)
+          costs[j] = j;
+        else {
+          if (j > 0) {
+            var newValue = costs[j - 1];
+            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+              newValue = Math.min(Math.min(newValue, lastValue),
+                costs[j]) + 1;
+            costs[j - 1] = lastValue;
+            lastValue = newValue;
+          }
+        }
+      }
+      if (i > 0)
+        costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+  }
+
+
+//   sample case
+let noun = "Gel";
+let string_test = "Jell"
+let score = similarity(noun.toLowerCase(), string_test.toLowerCase()); // score here
+
+console.log("similary test score: ", score);
