3df479e29fd842858ffef4529c8a7e10d0063d83
Kartikay Kapil <77982430+kartikay23@users.noreply.github.com>
2021-10-31 04:07:09 +0530

    Update sumavgarr.c

--- sumavgarr.c
+++ sumavgarr.c
@@ -1,10 +1,6 @@
 /* write a program find sum and average of given n elements
  in an array.
-
-NAME-KARTIKAY KAPIL
-ROLL NO. 31
-SECTION- F
-BRANCH- CSE         */
+*/
 
 #include<stdio.h>
 void main()
@@ -23,10 +19,8 @@ void main()
 }
 
 /*   OUTPUT
-kartikay@kartikay-HP-EliteBook-840-G2:~$ gcc sumavgarr.c
-kartikay@kartikay-HP-EliteBook-840-G2:~$ ./a.out
 enter the size of array:5
 enter the values:  2 34 5 7 2
-sum and average are: 50, 10
+sum and average are: 50, 10   */
 
 
