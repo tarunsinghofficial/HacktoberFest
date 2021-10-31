dc3d4bcecfefbc5d806ae39f5847fb785688b922
ArjunJadeja <contactarjunjadeja@gmail.com>
2021-10-31 08:07:35 +0530

    Commission Program Java

---
+++ Commission/Commission.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CommissionProgram {
+    public static void main(String[] args) {
+
+//      Calculate Commission Percentage
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Buying cost : ");
+        int buyingCost = input.nextInt();
+
+        System.out.print("Enter Selling price : ");
+        int sellingPrice = input.nextInt();
+
+        int difference = sellingPrice-buyingCost;
+        int commission = (buyingCost*difference)/100;
+
+        System.out.print("Commission : " + commission + " %");
+    }
+}
