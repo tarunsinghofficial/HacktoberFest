bbb4efb15249fe0f0cba19ddc8e7d1991fe44bb1
adeeb002 <96495920+adeeb002@users.noreply.github.com>
2022-10-10 18:49:33 +0530

    Update sortingarraynumber.c

--- C Program/sortingarraynumber.c
+++ C Program/sortingarraynumber.c
@@ -1,48 +1,41 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-	// selection sort(O(n^2))
-
-void ssort(int a[],int n)
-{	
-	for(int i=0;i<n-1;i++)
-	{
-		int p=i;
-		for(int j=i+1;j<n;j++)
-		{
-			if(a[j]<a[p])
-			{
-				p=j;	
-			}	
-		}
-		swap(a[i],a[p]);
-	}
+#include<stdio.h>
+void input(int a[],int n)
+{   int i;
+    printf("Enter array elements :\n");
+	for(i=0;i<n;i++)
+	scanf("%d",a+i);
 }
-
 void display(int a[],int n)
-{	
-	for(int i=0;i<n;i++)
-	{
-		cout<<a[i]<<" ";
-	}
+{   int i;
+	for(i=0;i<n;i++)
+	printf("%d  ",a[i]);
+ printf("\n");
 }
-
-
+void sort(int a[],int n)
+{  int i,j;
+   for(i=0;i<n;i++)
+   {
+   	 for(j=i+1;j<n;j++)
+   	 {
+   	 	if(a[i]>a[j])
+   	 	 { 
+   	 	    int t=a[i]; a[i]=a[j]; a[j]=t;
+			 }
+		}
+   }
+}
 int main()
 {
-	
-  // array input
-	
-  int n;
-	cout<<"enter the size of array :: ";
-	cin>>n;
+	int n,i;
+	printf("Enter size of array :\n");
+	scanf("%d",&n); 
 	int a[n];
-	for(int i=0;i<n;i++)
-	{
-		cin>>a[i];
-	}
-	
-	ssort(a,n);
-  display(a,n);
-  return 0;
+	printf("Enter array elements : \n");
+	input(a,n);
+	printf("Enter array elements : \n");
+	display(a,n);
+	sort(a,n);
+	printf("Sorted array : \n");
+	display(a,n);
+	return 0;
 }
