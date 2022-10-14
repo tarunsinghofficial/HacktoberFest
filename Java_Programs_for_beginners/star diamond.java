b1537e0af5db4ea50cc5e2aa0b989f8550c9ffd5
captainbkarthick <78250271+captainbkarthick@users.noreply.github.com>
2022-10-14 12:27:12 +0530

    remove the variable "space" from the program

--- Java_Programs_for_beginners/star diamond.java
+++ Java_Programs_for_beginners/star diamond.java
@@ -1,39 +1,32 @@
 import java.util.Scanner;  
 public class DiamondPattern  
 {  
-public static void main(String args[])  
-{  
-int row, i, j, space = 1;  
-System.out.print("Enter the number of rows you want to print: ");  
-Scanner sc = new Scanner(System.in);  
-row = sc.nextInt();  
-space = row - 1;  
-for (j = 1; j<= row; j++)  
-{  
-for (i = 1; i<= space; i++)  
-{  
-System.out.print(" ");  
-}  
-space--;  
-for (i = 1; i <= 2 * j - 1; i++)  
-{  
-System.out.print("*");  
-}  
-System.out.println("");  
-}  
-space = 1;  
-for (j = 1; j<= row - 1; j++)  
-{  
-for (i = 1; i<= space; i++)  
-{  
-System.out.print(" ");  
-}  
-space++;  
-for (i = 1; i<= 2 * (row - j) - 1; i++)  
-{  
-System.out.print("*");  
-}  
-System.out.println("");  
-}  
-}  
+public static void main(String[] args) {
+    int row, i, j, space = 1;
+    System.out.print("Enter the number of rows you want to print: ");
+    Scanner sc = new Scanner(System.in);
+    row = sc.nextInt();
+    for (int i = 0; i < row; i++) {
+      for (int j = i; j < row - 1; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k <= i; k++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+
+    row--;
+
+    for (int a = 0; a < row; a++) {
+      for (int b = 0; b <= a; b++) {
+        System.out.print(" ");
+      }
+      for (int c = a; c <= row - 1; c++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
 }  
