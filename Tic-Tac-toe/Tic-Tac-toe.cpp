37f7a1781d0fd71622fce09bf89bbbd69153d533
Shubham270301 <shubham27.sharma03@gmail.com>
2022-10-16 23:05:33 +0530

    Added Tic-Tac-toe.cpp file

---
+++ Tic-Tac-toe/Tic-Tac-toe.cpp
@@ -0,0 +1,79 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+ 
+// Driver Code
+int main()
+{  
+    // Stores the random numberr
+    int n,s;
+    char you, computer, result,k;
+    // Chooses the random number
+    // every time
+    srand(time(NULL));
+    // Make the random number less
+    // than 100, divided it by 100
+    n = rand() % 100; 
+    // Using simple probability 100 is
+    // roughly divided among stone,
+    // paper, and scissor
+    if (n < 33)
+        // s is denoting Stone
+        computer = 's';
+    else if (n > 33 && n < 66)
+        // p is denoting Paper
+        computer = 'p';
+    // z is denoting Scissor
+    else
+        computer = 'z';
+    printf("\n\n\n\n\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
+    // input from the user
+    scanf("%c", &you);
+    // If both the user and computer
+    // has chose the same thing
+    if (you == computer)
+        s= -1;
+ 
+    // If user's choice is stone and
+    // computer's choice is paper
+    if (you == 's' && computer == 'p')
+        s= 0;
+ 
+            // If user's choice is paper and
+            // computer's choice is stone
+            else if (you == 'p' && computer == 's') 
+              s = 1;
+ 
+    // If user's choice is stone and
+    // computer's choice is scissor
+    if (you == 's' && computer == 'z')
+        s = 1;
+ 
+    // If user's choice is scissor and
+    // computer's choice is stone
+    else if (you == 'z' && computer == 's')
+        s = 0;
+ 
+    // If user's choice is paper and
+    // computer's choice is scissor
+    if (you == 'p' && computer == 'z')
+        s = 0;
+ 
+    // If user's choice is scissor and
+    // computer's choice is paper
+    else if (you == 'z' && computer == 'p')
+        s= 1;
+    if (s == -1) {
+        printf("\n\n\t\t\t\tGame Draw!\n");
+    }
+    else if (s == 1) {
+        printf("\n\n\t\t\t\tWow! You have won the game!\n");
+    }
+    else {
+        printf("\n\n\t\t\t\tOh! You have lost the game!\n");
+    }
+        printf("\t\t\t\tYOu choose : %c and Computer choose : %c\n",you, computer);
+return 0;
+}
