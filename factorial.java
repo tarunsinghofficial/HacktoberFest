00f126dd8158a89ba6e9483bf111369cda83db39
parimal <97539350+parimalongithub@users.noreply.github.com>
2022-10-19 15:39:47 +0530

    added factorial program in java

---
+++ factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Please the enter the number that you have factorial of :");
+        long a=obj.nextInt();
+        long b=a;
+        long result=a;
+
+        for (int i=1;i<b;i++){
+            a--;
+             result =a*result;
+
+        }
+        System.out.println("The factorial of the number is :");
+        System.out.println(result);
+
+
+    }
+}
