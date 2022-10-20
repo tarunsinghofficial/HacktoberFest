9b92817af38884ad8297df4f542da8d476f5d792
Shreya <>
2022-10-20 11:46:08 -0700

    Added a new code for finding average

---
+++ Average.py
@@ -0,0 +1,6 @@
+Average
+#Find the average of two numbers 
+num1 = int(input("What is the first number? "))
+num2 = int(input("What is the second number?"))
+avg = num2 + num1
+print("The average is " + str(avg/2)
