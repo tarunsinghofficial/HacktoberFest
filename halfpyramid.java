d2112f4729e738e8186a3baf78176e8c0b6d71f7
unknown <kskkorada76@gmail.com>
2022-10-25 23:33:17 +0530

    added a java program

---
+++ halfpyramid.java
@@ -0,0 +1,15 @@
+public class halfpyramid{
+
+    public static void main(String args[]) {
+
+        for(int i =1; i<=5; i++){
+
+            for(int j=1; j<=i; j++){
+
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
