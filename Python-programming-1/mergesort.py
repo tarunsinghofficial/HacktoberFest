4efe9bc34026943277edcbc40fad39ad4b52b7c6
Saurabh <cse.19bcs6009@gmail.com>
2023-10-09 10:42:03 +0530

    added merge sort python program

---
+++ Python-programming-1/mergesort.py
@@ -0,0 +1,33 @@
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            nlist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            nlist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",nlist)
+
+nlist = [14,46,43,27,57,41,45,21,70]
+mergeSort(nlist)
+print(nlist)
