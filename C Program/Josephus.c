d0c9b8966aacf368456c4d60de78bbae50f59c95
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-10-12 10:21:03 +0530

    Update Josephus.c
    Added documentation

--- C Program/Josephus.c
+++ C Program/Josephus.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * This function calculates the survivor of the Josephus problem.
+ * 
+ * @param n The number of people standing in a circle.
+ * @param k The number of people to be skipped before an elimination.
+ * 
+ * @return The position of the survivor.
+ */
+
 int josephus(int n, int k) {
     if (n == 1) {
         return 1;
@@ -13,4 +22,3 @@ int main() {
     printf("The winner is %d\n", josephus(n, k));
     return 0;
 }
-
