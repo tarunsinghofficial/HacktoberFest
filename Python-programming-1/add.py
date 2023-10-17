ea5619ab993d5c0f9d5f0186abc6105425f50a85
Mohd Abbas Haidar <psibarpsi@gmail.com>
2023-10-18 00:46:03 +0530

    Update add.py
    Made the code more Pythonic and readable.

--- Python-programming-1/add.py
+++ Python-programming-1/add.py
@@ -1,5 +1,6 @@
-a=int(input("Enter the first number"))
-b=int(input("Enter the second number"))
-c=a+b
-print("the sum is",c)
-      
+userInput1 = int (input ('Enter the first number: '))
+userInput2 = int (input ('Enter the second number: '))
+
+result = userInput1 + userInput2
+
+print ('{operand1} + {operand2} = {finalResult}'.format (operand1 = userInput1, operand2 = userInput2, finalResult = result))
