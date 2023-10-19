4ae4073b7bbd7865a08e1921d9117e9e053df85c
Jyoti Rani <102273107+Jranii@users.noreply.github.com>
2023-10-19 16:48:34 +0530

    Add files via upload

---
+++ C Program/binary_search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1; // Target not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 12;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found in the array\n");
+
+    return 0;
+}
+
